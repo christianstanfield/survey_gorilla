7769c5663b1e8a6b51decaaa8e4aed124ccca646
& <pair+abbiecat.marinatedpork@devbootcamp.com>
2014-10-18 21:49:00 -0500

    working unstyled

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,4 @@
 get '/' do
-  erb :index
+  erb :survey_create
 end
 
---
+++ app/helpers/parse_survey_creation.rb
@@ -0,0 +1,22 @@
+class SurveyParser
+	def initialize
+	end
+
+	def parse(form_text)
+
+		input_array = form_text.split("&")
+		input_array.each do |pair|
+			key = pair.split("=")[0]
+			value = pair.split("=")[1]
+
+			case key
+			when 'title'
+				@current_survey = CreatedSurvey.create!(title: value)
+			when 'question'
+				@current_question = Question.create!(content: value, created_survey_id: @current_survey.id)
+			when 'choice'
+				Choice.create!(option: value, question_id: @current_question.id)
+			end
+		end
+	end
+end
