8a263400257ee76917c35d937d6075a77c31ebef
Aaron Piotrowski <a.paul.piotrowski@gmail.com>
2014-10-19 18:16:06 -0500

    fixed @user &&

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -30,7 +30,7 @@ end
 
 post '/users/login' do 
 	@user = User.find_by(email: params[:user][:email])
-	if @user.authenticate(params[:user][:password])
+	if @user && @user.authenticate(params[:user][:password])
 		session[:user_id] = @user.id
 		redirect "/users/#{@user.id}"
 	else
